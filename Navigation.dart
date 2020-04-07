002e77a2c43c8b1fc13cdac2721285ef1ec7c447
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2020-04-07 15:27:34 +0530

    Update Navigation.dart

--- Navigation.dart
+++ Navigation.dart
@@ -3,3 +3,11 @@ Navigator.popAndPushNamed(context, "/changepassword");
 /*In Main.dart file*/
 routes: {
       "/changepassword": (context) => ChangePassword(),}
+
+
+/*Kill current activity*/
+if (Navigator.canPop(context)) {
+  Navigator.pop(context);
+} else {
+  SystemNavigator.pop();
+}
